3b0792797e733c58c71f08fcb4dba1ed33457b53
mpatal <marvin.patal@hotmail.com>
2016-01-28 21:11:12 -0500

    Adding angularjs bower package and creating angular trip module and controller

---
+++ src/TheWorld/wwwroot/js/app-trips.js
@@ -0,0 +1,8 @@
+﻿//app-trips.js
+
+(function () {
+    "use strict";
+
+    //creating the app-trips module
+    angular.module("app-trips", []);
+})();
---
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,14 @@
+﻿(function() {
+    "use strict";
+
+    //Getting the eisting module and adding a controller
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController() {
+        var vm = this;
+
+        vm.name = "Marvin";
+    }
+
+})();
