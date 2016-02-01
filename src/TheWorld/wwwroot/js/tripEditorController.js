21506b69d8fa82dc9eee2fd859a028efedde1f41
mpatal <marvin.patal@hotmail.com>
2016-01-31 20:28:50 -0500

    Implement trip editor displaying the stops from the web api

--- src/TheWorld/wwwroot/js/tripEditorController.js
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -5,10 +5,26 @@
     angular.module("app-trips")
         .controller("tripEditorController", tripEditorController);
 
-    function tripEditorController() {
+    function tripEditorController($routeParams, $http) {
         var vm = this;
 
-        vm.name = "Marvin";
+        vm.tripName = $routeParams.tripName;
+        vm.stops = [];
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips/" + vm.tripName + "/stops")
+            .then(function(response) {
+                    //success
+                    angular.copy(response.data, vm.stops);
+                },
+                function(error) {
+                    //failure
+                    vm.errorMessage = "Failed to load stops";
+                })
+            .finally(function() {
+                vm.isBusy = false;
+            });
     };
 
 })();
