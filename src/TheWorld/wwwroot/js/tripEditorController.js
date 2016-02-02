076a3e38302c654fadc083959a884d872775d1d3
mpatal <marvin.patal@hotmail.com>
2016-02-01 21:47:51 -0500

    Add stops to trip, validate the form and refresh the map.

--- src/TheWorld/wwwroot/js/tripEditorController.js
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -12,8 +12,11 @@
         vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+        vm.newStop = {};
 
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        var url = "/api/trips/" + vm.tripName + "/stops";
+
+        $http.get(url)
             .then(function(response) {
                     //success
                     angular.copy(response.data, vm.stops);
@@ -34,6 +37,26 @@
             .finally(function() {
                 vm.isBusy = false;
             });
+
+        vm.addStop = function() {
+            vm.isBusy = true;
+
+            $http.post(url, vm.newStop)
+                .then(function(response) {
+                    //success go to route    
+                    vm.stops.push(response.data);
+                    _showTravelMap(vm.stops);
+
+                    //clear the form
+                    vm.newStop = {};
+                }, function(error) {
+                    //failure
+                    vm.errorMessage = "Failed to add new stop.";
+                })
+                .finally(function() {
+                    vm.isBusy = false;
+                });
+        };
     };
 
     function _showTravelMap(stops) {
